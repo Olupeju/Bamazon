1b8018c534a255990ab18f35f34148a1562ef010
unknown <olayika@hotmail.com>
2018-12-16 16:47:33 -0600

    file modified

--- schema.sql
+++ schema.sql
@@ -1,3 +1,4 @@
+DROP DATABASE if exists bamazonDB;
 CREATE DATABASE bamazonDB;
 
 USE bamazonDB;
@@ -13,24 +14,48 @@ PRIMARY KEY (item_id)
 );
 
 INSERT INTO products (product_name, department_name, price, stock_quantity, product_sales)
-VALUES ("Staple Clip (pkg. of 20)", "Office Supplies", 12.00, 50, 0.00),
+VALUES ("Staple Clip (pkg. of 20)", "Office Supplies", 12.00, 50, 4000.00),
 
-	   ("Blender", "Household Items", 15.00, 100, 0.00),
+	   ("Blender", "Household Items", 15.00, 100, 3000.00),
        
 	   ("Chair", "Furniture", 50.00, 25, 0.00),
        
-	   ("Dasani Water Bottles (pkg. of 25)", "Groceries", 32.00, 75, 0.00),
+	   ("Dasani Water Bottles (pkg. of 25)", "Groceries", 32.00, 75, 4500.00),
        
-	   ("Vacuum Cleaner", "Household Items", 340.00, 14, 0.00),
+	   ("Vacuum Cleaner", "Household Items", 340.00, 14, 4000.00),
        
 	   ("Pitch Perfect", "Media", 19.99, 87, 0.00),
        
-	   ("Starbucks K-Cups (pkg. of 20)", "Groceries", 7.50, 111, 0.00),
+	   ("Starbucks K-Cups (pkg. of 20)", "Groceries", 7.50, 111, 3500.00),
        
-	   ("Black Sharpie", "Office Supplies", 2.50, 200, 0.00),
+	   ("Black Sharpie", "Office Supplies", 2.50, 200, 3500.00),
        
-	   ("Scrabble", "Toys and Games", 13.50, 26, 0.00),
+	   ("Scrabble", "Toys and Games", 13.50, 26, 5000.00),
        
-	   ("End Table", "Household Items", 139.99, 19, 0.00);
+	   ("End Table", "Household Items", 139.99, 19, 4500.00);
 
 SELECT * FROM products;
+
+CREATE TABLE departments (
+	department_id INT AUTO_INCREMENT NOT NULL,
+  department_name VARCHAR(100) NULL,
+  over_head_costs DECIMAL(10,2) NULL,
+  PRIMARY KEY (department_id)
+);
+
+-- default table vals --
+INSERT INTO departments (department_name, over_head_costs)
+VALUES ("Groceries", 2000), ("Funitures", 2000), ("Office Supply", 10000), ("Toys and Games", 2000), ("Household Items", 1000), ("Media", 1000); 
+			 
+
+-- view tables --
+SELECT * FROM products;
+SELECT * FROM departments;
+
+-- inner join for profit query --
+SELECT department_id, departments.department_name, over_head_costs, SUM(product_sales) AS product_sales,
+	SUM(product_sales) - over_head_costs AS total_profit
+FROM departments
+INNER JOIN products
+ON departments.department_name = products.department_name
+GROUP BY department_id;
