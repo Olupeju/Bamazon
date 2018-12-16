f757c668746eb191312608052925663746a2aa94
unknown <olayika@hotmail.com>
2018-12-16 16:41:58 -0600

    updating the file

--- schema.sql
+++ schema.sql
@@ -13,15 +13,24 @@ PRIMARY KEY (item_id)
 );
 
 INSERT INTO products (product_name, department_name, price, stock_quantity, product_sales)
-VALUES ("Binder Clips (pkg. of 20)", "Office Supplies", "12.00", "50", "0.00"),
-	   ("Coffee Mug", "Household Items", "15.00", "100", "0.00"),
-	   ("100-Piece Puzzle", "Toys and Games", "9.00", "25", "0.00"),
-	   ("Dasani Water Bottles (pkg. of 25)", "Groceries", "32.00", "75", "0.00"),
-	   ("Dyson Vacuum Cleaner", "Household Items", "340.00", "14", "0.00"),
-	   ("Pitch Perfect", "Media", "19.99", "87", "0.00"),
-	   ("Starbucks K-Cups (pkg. of 20)", "Groceries", "7.50", "111", "0.00"),
-	   ("Black Sharpie", "Office Supplies", "2.50", "200", "0.00"),
-	   ("Scrabble", "Toys and Games", "13.50", "26", "0.00"),
-	   ("End Table", "Household Items", "139.99", "19", "0.00");
+VALUES ("Staple Clip (pkg. of 20)", "Office Supplies", 12.00, 50, 0.00),
 
+	   ("Blender", "Household Items", 15.00, 100, 0.00),
+       
+	   ("Chair", "Furniture", 50.00, 25, 0.00),
+       
+	   ("Dasani Water Bottles (pkg. of 25)", "Groceries", 32.00, 75, 0.00),
+       
+	   ("Vacuum Cleaner", "Household Items", 340.00, 14, 0.00),
+       
+	   ("Pitch Perfect", "Media", 19.99, 87, 0.00),
+       
+	   ("Starbucks K-Cups (pkg. of 20)", "Groceries", 7.50, 111, 0.00),
+       
+	   ("Black Sharpie", "Office Supplies", 2.50, 200, 0.00),
+       
+	   ("Scrabble", "Toys and Games", 13.50, 26, 0.00),
+       
+	   ("End Table", "Household Items", 139.99, 19, 0.00);
 
+SELECT * FROM products;
